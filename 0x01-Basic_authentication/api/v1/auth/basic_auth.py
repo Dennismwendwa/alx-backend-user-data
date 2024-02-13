30a399412748a15080968660bb10c246cc395dc9
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-13 14:52:18 +0300

    created decode and credentials etraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 from .auth import Auth
 from typing import Tuple, TypeVar
 import re
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -16,3 +18,31 @@ class BasicAuth(Auth):
             if all_matches is not None:
                 return all_matches.group("token")
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """This method decodes strings in base64"""
+        if isinstance(base64_authorization_header, str):
+            try:
+                string = base64.b64decode(base64_authorization_header,
+                                          validate=True)
+                return string.decode("utf-8")
+            except (binascii.Error, UnicodeDecodeError):
+                return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str,
+                                 ) -> Tuple[str, str]:
+        """This method gets the user credential details"""
+        if isinstance(decoded_base64_authorization_header, str):
+            pattern = r"(?P<user>[^:]+):(?P<password>.+)"
+            full_match = re.fullmatch(
+                pattern, decoded_base64_authorization_header.strip()
+            )
+            if full_match is not None:
+                current_user = full_match.group("user")
+                user_password = full_match.group("password")
+                return current_user, user_password
+        return None, None
