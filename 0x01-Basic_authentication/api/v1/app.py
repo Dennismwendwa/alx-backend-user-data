868d0a5215ea4634b88cfb0e4c6145e0bcdbb039
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-13 13:46:05 +0300

    created logic to authenticate paths

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,35 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+auth_type = os.getenv("AUTH_TYPE", "auth")
+if auth_type == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+if auth_type == "BasicAuth":
+    #auth = BasicAuth()
+    pass
+
+
+@app.before_request
+def before_request():
+    """This method runs before every call"""
+    if auth is None:
+        return
+
+    exclude_paths = ["/api/v1/status/",
+                     "/api/v1/unauthorized/",
+                     "/api/v1/forbidden/"]
+
+    if auth.require_auth(request.path,      
+                         exclude_paths):
+        auth_header = auth.authorization_header(request)
+        current_user = auth.current_user(request)
+        if auth_header is None:
+            abort(401)
+        if current_user is None:
+            abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -33,6 +62,7 @@ def forbidden_error(error) -> str:
     Raises forbbiden error view
     """
     return jsonify({"error": "Forbidden"}), 403
+
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """method authorized"""
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers["Authorization"]
 
     def current_user(self, request=None) -> TypeVar("User"):
         """method current user"""
