fc91d6642f690ee5645db5e384b36e42e006fa98
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-13 15:56:48 +0300

    updated auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,7 @@ from typing import List, TypeVar
 class Auth:
     """class auth for base authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """method require
+        """method require"""
 
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
@@ -29,6 +29,7 @@ class Auth:
                 if re.match(pattern, path):
                     return False
             return true
+        """
 
     def authorization_header(self, request=None) -> str:
         """method authorized"""
