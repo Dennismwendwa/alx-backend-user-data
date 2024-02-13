d4f610a9235e7af6b85fbae6681b4997cd9a35f8
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-13 09:20:28 +0300

    added unauthorized endpoint

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,9 +25,11 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route("/unauthorized/", methods=["GET"], strict_slashes=False)
 def unauthorized_endpoint() -> None:
     """GET /api/v1/unauthorized
     Return
-      - api status code"""
+      - api status code
+    """
     abort(401)
