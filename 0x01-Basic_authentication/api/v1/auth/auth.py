9c6cfa584a764fe2763a08036d41b7e6a6a78993
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-13 16:12:38 +0300

    pycodestyle correction

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,29 +13,15 @@ class Auth:
             return True
 
         path = path.rstrip("/")
-        
+
         for excluded_path in excluded_paths:
             excluded_path = excluded_path.rstrip("/")
-            if excluded_path.endswith("*") and path.startswith(excluded_path[:-1]):
+            if (excluded_path.endswith("*") and
+                    path.startswith(excluded_path[:-1])):
                 return False
             elif path == excluded_path:
                 return False
         return True
-        """
-        if path is not None and excluded_paths is not None:
-            for exc_p in map(lambda k: k.strip(), excluded_paths):
-                pattern = ""
-                if exc_p[-1] == "*":
-                    pattern = f"{exc_p[0:-1]}"
-                elif exc_p[-1] == "/":
-                    pattern = f"{exc_p[0:-1]}/"
-                else:
-                    pattern = f"{exc_p}/"
-
-                if re.match(pattern, path):
-                    return False
-            return true
-        """
 
     def authorization_header(self, request=None) -> str:
         """method authorized"""
