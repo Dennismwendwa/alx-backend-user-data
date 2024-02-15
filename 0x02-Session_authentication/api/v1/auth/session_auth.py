f0aa43f79f32b8c24128f84a49059b3e34cb87af
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-15 08:56:50 +0300

    created session class method to create user session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,21 @@
 from flask import request
 from models.user import User
 from .auth import Auth
-from uuid import uuid4
+import uuid
 
 
 class SessionAuth(Auth):
     """This is session class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """This method creates user session id"""
+        if isinstance(user_id, str):
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """This method takes session id and returns the user id"""
+        if isinstance(session_id, str):
+            return self.user_id_by_session_id.get(session_id)
