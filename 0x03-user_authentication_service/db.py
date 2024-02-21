9123230b0460c045fd827038d5cdf39f858db0b1
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-21 07:31:35 +0300

    updated imports

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+# from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from typing import Union
 
@@ -20,7 +22,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -37,13 +39,9 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """This methods adds uder to db"""
         if isinstance(email, str) and isinstance(hashed_password, str):
-            try:
-                user = User(email=email, hashed_password=hashed_password)
-                self._session.add(user)
-                self._session.commit()
-            except Exception:
-                self._session.rollback()
-                user = None
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
             return user
 
     def find_user_by(self, **kwargs) -> Union[User, None]:
