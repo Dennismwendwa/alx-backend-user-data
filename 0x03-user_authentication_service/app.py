328009731aef785fa02e2e6d0c300912ca01fd4a
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-22 06:03:56 +0300

    created login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -23,6 +23,26 @@ def register_users():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError as e:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """This route is for user login"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+
+            response = make_response(
+                jsonify({"email": email, "message": "logged in"}))
+            response.set_cookie("session_id", session_id)
+            return response
+        else:
+            abort(401)
+    except Exception:
+        abort(401)
 
 
 if __name__ == "__main__":
